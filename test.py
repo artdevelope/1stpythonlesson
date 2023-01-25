f31bbf4705ef207ea789cd34a8a168f6b0ce77e9
artdevelope <kondratenkoa029@gmail.com>
2023-01-25 14:31:38 +0300

    comment data type

--- test.py
+++ test.py
@@ -1,20 +1,58 @@
+#Типы данных
+
+#   'NoneType'
+#   'str'
+#   'list'
+#   'tuple'
+#   'set'
+#   'dict'
+#   'bool'
+
+# none - отсутствие данных
 a = None
 print(type(a))
+
+
+# 1 - целое число
 a = 1
 print(type(a))
+
+
+# 1 - число с плавающей точкой
 a = 1.0
 print(type(a))
+
+
+# 1 -   комплексное число
 a = 1 + 1j
 print(type(a))
+
+
+# 1 - строка
 a = '1'
 print(type(a))
+
+
+# 1 - список
 a = [1, 1, 'a']
 print(type(a))
+
+
+# 1 - кортеж
 a = (1, 1, 'a')
 print(type(a))
+
+
+# 1 - множество
 a = {1, 2, 'a'}
 print(type(a))
+
+
+# 1 - словарь
 a = {'a':1, 'b':2}
 print(type(a))
+
+
+# 1 - логические булевые значения
 a = True
 print(type(a))
